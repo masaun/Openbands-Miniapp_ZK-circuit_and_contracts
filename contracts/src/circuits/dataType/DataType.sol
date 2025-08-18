6c32b60d1efe65b30e2949d142431f835d291cf5
masaun <masaun@protonmail.com>
2025-08-18 17:59:33 +0900

    [SC]: Define the getNullifiersByDomainAndEmailHashAndWalletAddresses() to retrieve the nullifierHash by a caller's wallet address, domain, email hash

--- contracts/src/circuits/dataType/DataType.sol
+++ contracts/src/circuits/dataType/DataType.sol
@@ -9,6 +9,7 @@ library DataType {
         bytes32 nullifierHash;
 
         // @dev - Not genuine public inputs, which is emitted via zkJWT circuit. This is for the validation purpose.
+        string emailHash;
         address walletAddress;
         string createdAt;     // @dev - ISO String format (i.e. "2025-07-16T07:20:30.000Z")
         //uint256 createdAt;  // @dev - block.timestamp
