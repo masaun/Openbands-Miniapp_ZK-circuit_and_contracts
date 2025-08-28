43741ff8409891cb4c71c7661763b46d6653807a
masaun <masaun@protonmail.com>
2025-08-28 21:18:38 +0900

    [circuit]: Add the 'bb --version' command to the build.sh

--- circuits/build.sh
+++ circuits/build.sh
@@ -13,9 +13,11 @@ noirup --version 1.0.0-beta.6
 echo "Install the bb.js version v0.85.0..."
 bbup --version 0.85.0
 
-echo "Check the Noir/Nargo version and bb.js version of the local machine (This version is supposed to be v1.0.0-beta.6 / v0.85.0)..."
+echo "Check the Noir/Nargo version version of the local machine (This version is supposed to be v1.0.0-beta.6)..."
 nargo -V
-bb -V
+
+echo "Check the bb.js version of the local machine (This version is supposed to be v0.85.0)..."
+bb --version
 
 echo "Compiling circuit..."
 if ! nargo compile; then
